3f3a269b22891e135731b5c9472dbd1331bd5306
cychow <cchow@purdue.edu>
2016-12-08 21:52:56 -0500

    adding 5-7.bb8 to the repo so i can clone this tomorrow

--- wav2bb8/wav2bb8.m
+++ wav2bb8/wav2bb8.m
@@ -2,13 +2,16 @@
 clear
 clc
 close all;
-data = audioread('1.wav');
+data = audioread('7.wav');
+if size(data,2) == 2
+    data = (data(:,1)+data(:,2))./2;
+end
 data = data-min(data);
 data = data/max(data);
 data = data.*127;
 data = round(data);
 data = uint8(data);
 
-outputfile = fopen('1.bb8','wb');
+outputfile = fopen('7.bb8','wb');
 fwrite(outputfile,data);
 fclose(outputfile);
